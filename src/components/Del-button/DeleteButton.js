3ac8a31b69d5ba217d06f8e124a880a079307b4e
Inbar Danieli <inbar0308@gmail.com>
2021-12-07 11:26:36 +0200

    feat: delete button

---
+++ src/components/Del-button/DeleteButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DeleteButton(props){
+  // const items = props.items;
+function DelItems(){
+  
+}
+
+  return(<button onClick = {DelItems}>Del</button>)
+}
+
+export default DeleteButton
